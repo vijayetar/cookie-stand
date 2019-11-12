3bddcef9bd16989671d1ffb6aa2fd2ff51f05320
Vijayeta Rangarajan <vijayetar@gmail.com>
2019-11-11 19:44:54 -0800

    completed sales for Paris and Lima

--- app.js
+++ app.js
@@ -84,7 +84,7 @@ var tokyoShop = {
       this.cookiesEachHour.push(cookiesHourly);
       this.totalCookiesForTheDay += this.cookiesEachHour[i];
       // console.log('Tokyo Customer each hour: ', this.customersEachHour[i]);
-      console.log('Tokyo total Cookies for the Day: ', this.totalCookiesForTheDay);
+      // console.log('Tokyo total Cookies for the Day: ', this.totalCookiesForTheDay);
       // Identify the position where you want to add the content
       var position = document.getElementById('cookiesList');
       //createElement
@@ -157,4 +157,108 @@ var dubaiShop = {
 }
 
 dubaiShop.render();
+
+// create a sales list for Paris shop
+var parisShop = {
+  storeName: 'Paris',
+  minCustEachHour: 20,
+  maxCustEacHour: 38,
+  avgCookiesPerCustomer: 2.3,
+  customersEachHour: [],
+  cookiesEachHour: [],
+  totalCookiesForTheDay: 0,
+  render: function () {
+    // identify where you want to add the list and name the list
+    var position = document.getElementById('cookiesList');
+    var h2El = document.createElement('h2');
+    h2El.textContent = this.storeName;
+    position.appendChild(h2El);
+
+    for (var i = 0; i < hours.length; i++) {
+      // console.log('MAX: ',this.maxCustEacHour);
+      var difference = this.maxCustEacHour - this.minCustEachHour;
+      var rndNumber = Math.round((Math.floor(Math.random() * (difference + 1)) + this.minCustEachHour));
+      // console.log(rndNumber);
+      this.customersEachHour.push(rndNumber);
+      var cookiesHourly = this.customersEachHour[i] * this.avgCookiesPerCustomer;
+      // console.log('hourly cookies', cookiesHourly);
+      cookiesHourly = Math.round(cookiesHourly);
+      this.cookiesEachHour.push(cookiesHourly);
+      this.totalCookiesForTheDay += this.cookiesEachHour[i];
+      // console.log('Customer each hour: ', this.customersEachHour[i]);
+      // console.log('Total Cookies for the Day: ', this.totalCookiesForTheDay);
+
+      // now to add the calculated numbers to the list in the DOM
+      //first createElement
+      var liEl = document.createElement('li');
+      //second create text content
+      liEl.textContent = `${hours[i]} ${this.storeName} Cookies per Hour : ${this.cookiesEachHour[i]}`;
+      //now append to the document by using .appendChild(liEl)
+      position.appendChild(liEl);
+    }
+    //createElement
+    var liEl = document.createElement('li');
+    //create text content
+    liEl.textContent = `Total Cookies per Hour : ${this.totalCookiesForTheDay}`;
+    //append to the document by using .appendChild(liEl)
+    position.appendChild(liEl);
+
+  }
+
+}
+
+parisShop.render();
+
+// create a sales list for Lima shop
+var limaShop = {
+  storeName: 'Lima',
+  minCustEachHour: 2,
+  maxCustEacHour: 16,
+  avgCookiesPerCustomer: 4.6,
+  customersEachHour: [],
+  cookiesEachHour: [],
+  totalCookiesForTheDay: 0,
+  render: function () {
+    // identify where you want to add the list and name the list
+    var position = document.getElementById('cookiesList');
+    var h2El = document.createElement('h2');
+    h2El.textContent = this.storeName;
+    position.appendChild(h2El);
+
+    for (var i = 0; i < hours.length; i++) {
+      // console.log('MAX: ',this.maxCustEacHour);
+      var difference = this.maxCustEacHour - this.minCustEachHour;
+      var rndNumber = Math.round((Math.floor(Math.random() * (difference + 1)) + this.minCustEachHour));
+      // console.log(rndNumber);
+      this.customersEachHour.push(rndNumber);
+      var cookiesHourly = this.customersEachHour[i] * this.avgCookiesPerCustomer;
+      // console.log('hourly cookies', cookiesHourly);
+      cookiesHourly = Math.round(cookiesHourly);
+      this.cookiesEachHour.push(cookiesHourly);
+      this.totalCookiesForTheDay += this.cookiesEachHour[i];
+      // console.log('Customer each hour: ', this.customersEachHour[i]);
+      // console.log('Total Cookies for the Day: ', this.totalCookiesForTheDay);
+
+      // now to add the calculated numbers to the list in the DOM
+      //first createElement
+      var liEl = document.createElement('li');
+      //second create text content
+      liEl.textContent = `${hours[i]} ${this.storeName} Cookies per Hour : ${this.cookiesEachHour[i]}`;
+      //now append to the document by using .appendChild(liEl)
+      position.appendChild(liEl);
+    }
+    //createElement
+    var liEl = document.createElement('li');
+    //create text content
+    liEl.textContent = `Total Cookies per Hour : ${this.totalCookiesForTheDay}`;
+    //append to the document by using .appendChild(liEl)
+    position.appendChild(liEl);
+
+  }
+
+}
+
+limaShop.render();
+
+
 
